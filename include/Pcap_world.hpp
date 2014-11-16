22bd42bab617d1a69f21fb34778823545f8a749a
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2014-11-15 22:20:53 -0800

    win32 sockets support added

--- include/Pcap_world.hpp
+++ include/Pcap_world.hpp
@@ -1,5 +1,15 @@
 #pragma once
 
+#if defined( _WIN32 )
+#include <WS2tcpip.h>
+#include <Windows.h>
+#include <Iphlpapi.h>
+#pragma comment (lib,"Ws2_32.lib")
+#pragma comment (lib,"Iphlpapi.lib")
+#else
+#include <sys/time.h>
+#endif
+
 #include <iostream>
 #include <iomanip>
 #include <string>
