622c495176325e0f89f0803366ceadbc6b33b51c
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2014-11-27 11:07:32 -0800

    Move software dirs into software dir

---
+++ include/Pcap_world.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#if defined( _WIN32 )
+#include <WS2tcpip.h>
+#include <Windows.h>
+#include <Iphlpapi.h>
+#pragma comment (lib,"Ws2_32.lib")
+#pragma comment (lib,"Iphlpapi.lib")
+#else
+#include <sys/time.h>
+#endif
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <memory>
+#include <stdexcept>
+#include <cstdlib>
+#include <cstring>
